53b5e97371863eb5d94737c03262ba5ce2ff08f0
Roly Perera <roly.perera@dynamicaspects.org>
2026-05-15 12:01:05 +0100

    Basic well-formed mutual block.

---
+++ test/well-formed/functions/mutual_recursion.py
@@ -0,0 +1,12 @@
+def even(n):
+    if n == 0:
+        return True
+    return odd(n - 1)
+
+def odd(n):
+    if n == 0:
+        return False
+    return even(n - 1)
+
+print(even(10))
+print(odd(10))
